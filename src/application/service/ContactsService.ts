84edfcf4b846b1c9b753c558a50374a4722bb10f
Laycon Gomes <laycon.gomes@tld.com.br>
2025-05-15 19:30:18 -0300

    feat: create ContactService

--- src/application/service/ContactsService.ts
+++ src/application/service/ContactsService.ts
@@ -1,3 +1,48 @@
-export default class ProcessPersonService {
-    
+import People from "../../domain/entity/People";
+
+export default class PersonService {
+    process(people: People[]){
+        const filteredPeople = people.slice(1).filter((v) => {
+        return v?.name && v?.getPhone() ;
+        });
+        console.log("Pessoas com dados válidos: ", filteredPeople.length);
+
+        const mobilePhone = filteredPeople.map(((value, i) => {
+            try {
+                const onlyNumberPhone = value?.getPhone()?.replace(/[^0-9]/g,'');  
+                
+                if(onlyNumberPhone.length == 11) {
+                    //73988675742
+                    return {
+                        name: value.name,
+                        phone: `55${value.getPhone()}`
+                    }
+                } else if (onlyNumberPhone.length == 10) {
+                    return {
+                        name: value.name,
+                        phone: `55${value.getPhone().slice(0, 2)}9${value.getPhone().slice(2)}`
+                    }
+                } else {
+                    console.log(onlyNumberPhone.length)
+                    return null
+                }
+
+            } catch (error) {
+                console.log("Erro ao processar ", value, error);
+                return null
+            }
+        }))
+
+        const numbers = ['2', '3', '4', '5']
+        const filteredData = mobilePhone.filter(value => {
+            try {
+                
+                return value && !numbers.includes(value.phone[5]);
+            } catch (error) {
+                return null;
+            }
+        });
+
+        return filteredData;
+    }
 }
